97dca48113e0990fd5bdb7904275923489a9b456
TheoEC <theo_portela@hotmail.com>
2021-07-30 17:55:56 -0300

    iniciando repositorio

---
+++ dicionario.py
@@ -0,0 +1,6 @@
+class dicionario:
+    estadoDest = "end-entrega-estado"
+    tipoPag    = "pagamento-nome"
+    status     = "pedido-situacao"
+    metEnvio   = "envio-nome"
+    entregue   = "Pedido Entregue"
